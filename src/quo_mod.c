1319c76468cac597b816b6eac37fd2d76fc485a5
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-14 19:17:11 +0000

    Use const

--- src/quo_mod.c
+++ src/quo_mod.c
@@ -7,7 +7,7 @@
 #include "quo_mod.h"
 
 struct quo_mod quo_mod(int x, int y) {
-  int mod = x % y;
+  const int mod = x % y;
   /*
    * Returns a quo_mod structure by casting an initialiser. Is this portable?
    */
