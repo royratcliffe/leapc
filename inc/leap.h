897261373ca14f7cb7f88db89d18ea7f2a73461e
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-14 12:18:29 +0000

    Copyright 2025

--- inc/leap.h
+++ inc/leap.h
@@ -2,7 +2,7 @@
 /*!
  * \file leap.h
  * \brief Leap year function prototypes.
- * \copyright 2024, 2025, Roy Ratcliffe, Northumberland, United Kingdom
+ * \copyright 2025, Roy Ratcliffe, Northumberland, United Kingdom
  */
 #pragma once
 
