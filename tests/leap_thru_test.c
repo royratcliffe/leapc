07c13c6ca9b366c1d80b380d4378cc76d7666333
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-16 11:47:13 +0000

    Add additional assertions to leap_thru_test for years 0-3 inclusive

--- tests/leap_thru_test.c
+++ tests/leap_thru_test.c
@@ -3,6 +3,10 @@
 #include <assert.h>
 
 int leap_thru_test(int argc, char **argv) {
+  assert(0 == leap_thru(0));
+  assert(0 == leap_thru(1));
+  assert(0 == leap_thru(2));
+  assert(0 == leap_thru(3));
   assert(1 == leap_thru(4));
   assert(24 == leap_thru(100));
   assert(97 == leap_thru(400));
