26e9144bbab95233ecdd2c0978a60ee0cc51fd12
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-18 14:45:13 +0000

    Add additional assertions to leap_off_test for comprehensive coverage

--- tests/leap_off_test.c
+++ tests/leap_off_test.c
@@ -22,5 +22,9 @@ int leap_off_test(int argc, char **argv) {
   assert(equal_leap_off((struct leap_off){4, 365}, leap_off(5, -1)));
   assert(equal_leap_off((struct leap_off){5, 0}, leap_off(4, 366)));
   assert(equal_leap_off((struct leap_off){2024, 365}, leap_off(2024, 365)));
+  assert(equal_leap_off((struct leap_off){2025, 0}, leap_off(2024, 366)));
+  assert(equal_leap_off((struct leap_off){ 1902, 270 }, leap_off(1900, 1000)));
+  assert(equal_leap_off((struct leap_off){ 1900, 0 }, leap_off(0, LEAP_MCM)));
+  assert(equal_leap_off((struct leap_off){ 1970, 0 }, leap_off(0, leap_day(1970))));
   return EXIT_SUCCESS;
 }
