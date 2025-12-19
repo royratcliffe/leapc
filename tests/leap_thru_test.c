3f3f77c338db41bd57eba4ec10192e68e5468de8
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-19 07:38:11 +0000

    Add additional assertions to leap_thru_test for comprehensive coverage

--- tests/leap_thru_test.c
+++ tests/leap_thru_test.c
@@ -8,8 +8,13 @@ int leap_thru_test(int argc, char **argv) {
   assert(0 == leap_thru(2));
   assert(0 == leap_thru(3));
   assert(1 == leap_thru(4));
+  assert(1 == leap_thru(5));
   assert(24 == leap_thru(100));
+  assert(24 == leap_thru(101));
+  assert(48 == leap_thru(200));
+  assert(48 == leap_thru(201));
   assert(97 == leap_thru(400));
+  assert(97 == leap_thru(401));
   assert(693960 == 1900 * 365 + leap_thru(1900));
   return 0;
 }
