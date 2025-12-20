df99082a01404ebde49b1935fb4d89fe03a250af
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-20 06:30:40 +0000

    Use const for quo_mod variables in leap_mday and leap_yday functions

--- src/leap.c
+++ src/leap.c
@@ -78,13 +78,13 @@ struct leap_off leap_off(int year, int day) {
 
 int leap_mday(int year, int month) {
   static const int MDAY[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-  struct quo_mod qm = quo_mod(month - 1, 12);
+  const struct quo_mod qm = quo_mod(month - 1, 12);
   return MDAY[qm.mod] + (month == 2 ? leap_add(year + qm.quo) : 0);
 }
 
 int leap_yday(int year, int month) {
   static const int YDAY[] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
-  struct quo_mod qm = quo_mod(month - 1, 12);
+  const struct quo_mod qm = quo_mod(month - 1, 12);
   return YDAY[qm.mod] + (month > 2 ? leap_add(year + qm.quo) : 0);
 }
 
