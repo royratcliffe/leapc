2cda70e5936d6efd526c435898769af9505ae5af
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-20 06:58:58 +0000

    Add additional test case for leap_from function to verify edge date handling

--- tests/leap_from_test.c
+++ tests/leap_from_test.c
@@ -13,6 +13,7 @@ int leap_from_test(int argc, char **argv) {
 
   assert(equal_leap_date((struct leap_date){2024, 1, 1}, leap_date_from_off(leap_from(2024, 1, 1))));
   assert(equal_leap_date((struct leap_date){2023, 12, 1}, leap_date_from_off(leap_from(2024, 0, 1))));
+  assert(equal_leap_date((struct leap_date){2023, 11, 30}, leap_date_from_off(leap_from(2024, 0, 0))));
 
   return EXIT_SUCCESS;
 }
